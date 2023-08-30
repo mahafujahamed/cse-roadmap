6aaa6c728b0585d2af6eb5baeb16796ce0b28f61
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-08-31 00:03:12 +0600

    adds auto typing header text

---
+++ index.js
@@ -0,0 +1,26 @@
+const textElement = document.getElementById('typing-text');
+const textToType = 'Welcome tech world!';
+let index = 0;
+
+function typeText() {
+    if (index < textToType.length) {
+        textElement.textContent += textToType.charAt(index);
+        index++;
+        setTimeout(typeText, 100); // Adjust typing speed here (in milliseconds)
+    } else {
+        setTimeout(eraseText, 1000); // Delay before erasing text (in milliseconds)
+    }
+}
+
+function eraseText() {
+    if (index > 0) {
+        textElement.textContent = textToType.substring(0, index - 1);
+        index--;
+        setTimeout(eraseText, 50); // Adjust erasing speed here (in milliseconds)
+    } else {
+        index = 0;
+        setTimeout(typeText, 1000); // Delay before starting to type again (in milliseconds)
+    }
+}
+
+typeText();
